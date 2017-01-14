daf231f552d53691bf0d1056c2351f3cf123647b
William Tumeo <tumeowilliam@gmail.com>
2017-01-14 21:58:07 -0200

    Add tiled sky scene

--- scripts/core/SkyTileMap.gd
+++ scripts/core/SkyTileMap.gd
@@ -1,6 +1,7 @@
 extends TileMap
 
-var offset = 2
+export(String) var target_group = 'balloon'
+export(int) var offset = 2
 var current_tile = 0
 var last_center
 
@@ -12,18 +13,18 @@ func _fixed_process(delta):
 	fill_tiles()
 
 func fill_tiles():
-	var balloons = get_tree().get_nodes_in_group('balloon')
+	var balloons = get_tree().get_nodes_in_group(target_group)
 	if balloons.size() > 0:
 		var center = world_to_map(balloons[0].get_global_pos())
 		if last_center == center:
 			return
-			
+		
 		last_center = center
 		var centerx = center.x
 		var centery = center.y
+		# TODO: Improve clear method
 		clear()
 		for x in range(centerx-offset, centerx+offset):
 			for y in range(centery-offset, centery+offset):
 				print('ADD TILE TO: ', x, ', ', y)
 				set_cell(x, y, current_tile)
-				
