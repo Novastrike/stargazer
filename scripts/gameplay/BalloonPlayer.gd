c69a1938248a50f5a06d41c758f79dd389c02282
William Tumeo <tumeowilliam@gmail.com>
2017-01-15 21:35:58 -0200

    Fix front balloon collision

--- scripts/gameplay/BalloonPlayer.gd
+++ scripts/gameplay/BalloonPlayer.gd
@@ -6,16 +6,17 @@ func _ready():
 	var balloons = get_tree().get_nodes_in_group('balloon-body')
 	randomize()
 	random_balloon = balloons[int(rand_range(0, balloons.size()))]
+	random_balloon.add_to_group('front-balloon')
 	random_balloon.set_collision_mask(2)
 	random_balloon.set_layer_mask(2)
 	random_balloon.set_z(random_balloon.get_z()+1)
 	
 	for balloon in balloons:
-		if balloon != random_balloon:
+		if not 'front-balloon' in balloon.get_groups():
 			balloon.connect('explode', self, '_on_balloon_explode')
-	
 
 func _on_balloon_explode():
-	if random_balloon != null:
-		random_balloon.set_collision_mask(1)
-		random_balloon.set_layer_mask(1)
+	var front_balloon = get_tree().get_nodes_in_group('front-balloon')
+	if front_balloon.size() > 0:
+		front_balloon[0].set_collision_mask(1)
+		front_balloon[0].set_layer_mask(1)
