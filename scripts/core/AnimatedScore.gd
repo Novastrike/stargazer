322d849204d2be1880700d72e11f6a3382207b1d
William Tumeo <tumeowilliam@gmail.com>
2017-01-27 21:30:35 -0200

    Add Sfx to AnimatedScore

--- scripts/core/AnimatedScore.gd
+++ scripts/core/AnimatedScore.gd
@@ -5,6 +5,8 @@ export(int) var value = 0 setget _update_value
 export(bool) var show_zero = false
 export(String) var prefix = ''
 export(String) var suffix = ''
+export(bool) var enable_sfx = true
+export(String) var sfx_name = ''
 
 onready var tween = get_node("Tween")
 
@@ -13,11 +15,16 @@ func _ready():
 
 func _update_value(val):
 	value = int(val)
+	update_value()
+
+func update_value():
 	if value == 0 and not show_zero:
 		set_text('')
 	else:
 		set_text(prefix + str(value) + suffix)
 		
+	if enable_sfx:
+		get_node("Sfx").play(sfx_name)
 
 func interpolate_from_to(from, to):
 	tween.interpolate_property(self, 'value', from, to, 1, 0, 0)
@@ -25,3 +32,7 @@ func interpolate_from_to(from, to):
 
 func add(val):
 	interpolate_from_to(value, value+val)
+	
+func sub(val):
+	interpolate_from_to(value, value-val)
+	
