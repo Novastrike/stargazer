9eb73ef0d111134abb5dd4888406b431c68251bf
William Tumeo <tumeowilliam@gmail.com>
2017-01-31 02:47:21 -0200

    Add Start game animations

--- scripts/core/GameScreen.gd
+++ scripts/core/GameScreen.gd
@@ -1,15 +1,15 @@
 extends Node2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	start_game()
-	pass
+	set_process(true)
 
+
+func _process(delta):
+	if Input.is_action_pressed("ui_right") and Input.is_action_just_pressed("ui_left"):
+		print('START GAEM')
+		start_game()
+		set_process(false)
 
 func start_game():
 	get_node("MainLayer/BalloonPlayer").unlock()
