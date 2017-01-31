b4123d2449b98b7a35d37e9f188a072640f349ba
William Tumeo <tumeowilliam@gmail.com>
2017-01-31 08:32:21 -0200

    Add spawn to game scene

--- scripts/core/GameScreen.gd
+++ scripts/core/GameScreen.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 
 func _ready():
+	print(OS.get_cmdline_args())
 	set_process(true)
 
 
@@ -14,3 +15,7 @@ func _process(delta):
 func start_game():
 	get_node("MainLayer/BalloonPlayer").unlock()
 	get_node("AnimationPlayer").play("start_game")
+
+func _on_AnimationPlayer_finished():
+	if get_node("HUD/GameHUD/AnimationPlayer").get_current_animation() == 'start':
+		get_node("MainLayer/SpawnerTimer").start()
